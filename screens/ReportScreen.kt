7fe2f6b54c84cff9491cabbb3442751a200dccdd
cinthiayocelynavinarivera <24170032@uttcampus.edu.mx>
2026-03-24 12:19:14 -0600

    Lista Pantalla Reportes

--- screens/ReportScreen.kt
+++ screens/ReportScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -264,4 +265,10 @@ fun ProductReportRow(product: ProductItem) {
             }
         }
     }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ReportPreview (){
+    ReportScreen()
 }
--- view_models/ProductViewModel.kt
+++ view_models/ProductViewModel.kt
@@ -18,108 +18,107 @@ import kotlinx.coroutines.launch
 
 
 class ProductViewModel : ViewModel() {
-    private val  _uiState = MutableStateFlow(ProductUIState())
-    val uiState: StateFlow<ProductUIState> = _uiState.asStateFlow()
+   private val  _uiState = MutableStateFlow(ProductUIState())
+   val uiState: StateFlow<ProductUIState> = _uiState.asStateFlow()
 
-    private val _categoriesList: MutableStateFlow<List<Category>> = MutableStateFlow(emptyList())
-    val categoriesList: StateFlow<List<Category>> = _categoriesList.asStateFlow()
-    private val _suppliersList: MutableStateFlow<List<Supplier>> = MutableStateFlow(emptyList())
-    val suppliersList: StateFlow<List<Supplier>> = _suppliersList.asStateFlow()
+   private val _categoriesList: MutableStateFlow<List<Category>> = MutableStateFlow(emptyList())
+   val categoriesList: StateFlow<List<Category>> = _categoriesList.asStateFlow()
+   private val _suppliersList: MutableStateFlow<List<Supplier>> = MutableStateFlow(emptyList())
+   val suppliersList: StateFlow<List<Supplier>> = _suppliersList.asStateFlow()
 
-    val repo = PharmacyRepository
+   val repo = PharmacyRepository
 
 
-    private fun ProductUIState.toProductRequest(): ProductRequest{
-        return ProductRequest(
-            codigo = this.codigo,
-            name = this.name,
-            presentation = this.presentation,
-            purchase_price = this.purchase_price,
-            sale_price = this.sale_price,
-            stock = this.stock,
-            location = this.location,
-            min_stock = this.min_stock,
-            max_stock = this.max_stock,
-            description = this.description,
-            image = this.image,
-            category_id = this.category_id,
-            supplier_id= this.supplier_id
-        )
-    }
+   private fun ProductUIState.toProductRequest(): ProductRequest {
+      return ProductRequest(
+         codigo = this.codigo,
+         name = this.name,
+         presentation = this.presentation,
+         purchase_price = this.purchase_price,
+         sale_price = this.sale_price,
+         stock = this.stock,
+         location = this.location,
+         min_stock = this.min_stock,
+         max_stock = this.max_stock,
+         description = this.description,
+         image = this.image,
+         category_id = this.category_id,
+         supplier_id= this.supplier_id
+      )
+   }
 
-    fun createProduct(){
-        viewModelScope.launch {
+   fun createProduct(){
+      viewModelScope.launch {
 
-            try {
-                val request : ProductRequest = _uiState.value.toProductRequest()
-                val newProduct: Product = repo.addProducts(request)
+         try {
+            val request : ProductRequest = _uiState.value.toProductRequest()
+            val newProduct: Product = repo.addProducts(request) // a revisar
 
-            }catch (e:Exception){
+         }catch (e:Exception){
 
-            }
-        }
-    }
-    fun getCategories(){
-        viewModelScope.launch {
-            try{
-                val categories = repo.getCategories()
-                println("CATEGORIAS RECIBIDAS: ${categories.size}") // Mira esto en el Logcat
-                _categoriesList.value = categories
-            }catch (e: Exception){
-                println("ERROR API: ${e.message}") // Mira si sale error aquí
-            }
-        }
-    }
-    fun getSuppliers(){
-        viewModelScope.launch {
-            try{
-                val suppliers = repo.getSupplier()
-                _suppliersList.value = suppliers
-            }catch(e: Exception){
+         }
+      }
+   }
+   fun getCategories(){
+      viewModelScope.launch {
+         try{
+            val categories = repo.getCategories()
+            println("CATEGORIAS RECIBIDAS: ${categories.size}") // Mira esto en el Logcat
+            _categoriesList.value = categories
+         }catch (e: Exception){
+            println("ERROR API: ${e.message}") // Mira si sale error aquí
+         }
+      }
+   }
+   fun getSuppliers(){
+      viewModelScope.launch {
+         try{
+            val suppliers = repo.getSupplier()
+            _suppliersList.value = suppliers
+         }catch(e: Exception){
 
-            }
-        }
-    }
-    fun onValueCode(codigo:String){
-        _uiState.value = _uiState.value.copy(codigo=codigo)
-    }
-    fun onValueName(name:String){
-        _uiState.value = _uiState.value.copy(name=name)
-    }
-    fun onValuePresentation(presentation: String){
-        _uiState.value = _uiState.value.copy(presentation=presentation)
-    }
-    fun onValuePurchase(purchase:String){
-        var Purchase = purchase.toDoubleOrNull()?:0.0
-        _uiState.update { it.copy(purchase_price = Purchase) }
-    }
-    fun onValueSale(sale:String){
-        var Sale = sale.toDoubleOrNull()?:0.0
-        _uiState.update { it.copy(sale_price = Sale) }
-    }
-    fun onValueLocation(location: String){
-        _uiState.value = _uiState.value.copy(location=location)
-    }
+         }
+      }
+   }
+   fun onValueCode(codigo:String){
+      _uiState.value = _uiState.value.copy(codigo=codigo)
+   }
+   fun onValueName(name:String){
+      _uiState.value = _uiState.value.copy(name=name)
+   }
+   fun onValuePresentation(presentation: String){
+      _uiState.value = _uiState.value.copy(presentation=presentation)
+   }
+   fun onValuePurchase(purchase:String){
+      var Purchase = purchase.toDoubleOrNull()?:0.0
+      _uiState.update { it.copy(purchase_price = Purchase) }
+   }
+   fun onValueSale(sale:String){
+      var Sale = sale.toDoubleOrNull()?:0.0
+      _uiState.update { it.copy(sale_price = Sale) }
+   }
+   fun onValueLocation(location: String){
+      _uiState.value = _uiState.value.copy(location=location)
+   }
 
-    fun onValueMinStock(minStock:String){
-        var MinStock = minStock.toIntOrNull()?:0
-        _uiState.update { it.copy(min_stock = MinStock ) }
-    }
-    fun onValueMaxStock(maxStock:String){
-        var MaxStock = maxStock.toIntOrNull()?:0
-        _uiState.update { it.copy(max_stock = MaxStock) }
-    }
-    fun onValueDescription(description: String){
-        _uiState.value = _uiState.value.copy(description=description)
-    }
-    fun onValueCategory(category: Int){
+   fun onValueMinStock(minStock:String){
+      var MinStock = minStock.toIntOrNull()?:0
+      _uiState.update { it.copy(min_stock = MinStock ) }
+   }
+   fun onValueMaxStock(maxStock:String){
+      var MaxStock = maxStock.toIntOrNull()?:0
+      _uiState.update { it.copy(max_stock = MaxStock) }
+   }
+   fun onValueDescription(description: String){
+      _uiState.value = _uiState.value.copy(description=description)
+   }
+   fun onValueCategory(category: Int){
 
-        _uiState.update { it.copy(category_id = category) }
-    }
-    fun onValueSupplier(supplier: Int){
+      _uiState.update { it.copy(category_id = category) }
+   }
+   fun onValueSupplier(supplier: Int){
 
-        _uiState.update { it.copy(supplier_id = supplier) }
-    }
-
+      _uiState.update { it.copy(supplier_id = supplier) }
+   }
 
 }
